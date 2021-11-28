123b555196c6991875242be2a0d4ea49c62f1abf
Igor <ig.vp@hotmail.com>
2021-11-28 17:29:16 -0300

    title and inputs done

--- src/components/LoginPage/index.tsx
+++ src/components/LoginPage/index.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-import { LoginPageWarper, LoginPageImageWarper, LoginPageImage } from "./style";
+import {
+  LoginPageWarper,
+  LoginPageImageWarper,
+  LoginPageImage,
+  LoginPageFormWarper,
+  LoginPageFormContainer,
+  LoginPageFormTitle,
+  LoginPageFormItemWarper,
+  LoginPageFormItemTitle,
+  LoginPageFormItem,
+} from "./style";
 import ImagePath from "../../assets/background.jpeg";
-/*     */
+/*
+ */
 const LoginPage: React.FC = () => {
   return (
     <LoginPageWarper>
       <LoginPageImageWarper>
         <LoginPageImage src={ImagePath} />
       </LoginPageImageWarper>
+      <LoginPageFormWarper>
+        <LoginPageFormContainer>
+          <LoginPageFormTitle>Welcome to Jammy</LoginPageFormTitle>
+          <LoginPageFormItemWarper>
+            <LoginPageFormItemTitle>Full Name</LoginPageFormItemTitle>
+            <LoginPageFormItem placeholder="John Doe"></LoginPageFormItem>
+          </LoginPageFormItemWarper>
+          <LoginPageFormItemWarper>
+            <LoginPageFormItemTitle>Email</LoginPageFormItemTitle>
+            <LoginPageFormItem placeholder="Enter your Email here"></LoginPageFormItem>
+          </LoginPageFormItemWarper>
+          <LoginPageFormItemWarper>
+            <LoginPageFormItemTitle>Password</LoginPageFormItemTitle>
+            <LoginPageFormItem placeholder="Enter your Password"></LoginPageFormItem>
+          </LoginPageFormItemWarper>
+        </LoginPageFormContainer>
+      </LoginPageFormWarper>
     </LoginPageWarper>
   );
 };
--- src/components/LoginPage/style.tsx
+++ src/components/LoginPage/style.tsx
@@ -7,12 +7,54 @@ export const LoginPageWarper = styled.div`
   flex-direction: row;
 `;
 export const LoginPageImageWarper = styled.div`
-  width: 40%;
-  background-color: blue;
+  width: 45%;
 `;
 
 export const LoginPageImage = styled.img`
-  overflow: hidden;
+  object-fit: cover;
   width: 100%;
   height: 100vh;
+`;
+
+export const LoginPageFormWarper = styled.div`
+  width: 55%;
+  height: 100vh;
+  background-color: ${(props) => props.theme.colors.background};
+  padding: 5%;
+`;
+
+export const LoginPageFormContainer = styled.form`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const LoginPageFormTitle = styled.h1`
+  font-size: 48px;
+  font-weight: 600;
+  margin-bottom: 8%;
+`;
+export const LoginPageFormItemWarper = styled.div`
+  width: 100%;
+  height: 150px;
+`;
+
+export const LoginPageFormItemTitle = styled.p`
+  font-size: 20px;
+  color: ${(props) => props.theme.colors.secondary};
+  margin-bottom: 5px;
+`;
+
+export const LoginPageFormItem = styled.input`
+  width: 100%;
+  height: 65px;
+  background-color: ${(props) => props.theme.colors.backgroundInput};
+  outline: none;
+  border: none;
+  color: ${(props) => props.theme.colors.secondary};
+  padding-top: 3%;
+  padding-bottom: 3%;
+  padding-left: 5%;
+  font-size: 20px;
 `;
